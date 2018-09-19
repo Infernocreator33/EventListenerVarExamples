b980563bd0561e3797c7c12f5f475751dae97360
Infernocreator33 <zs0877310@otc.edu>
2018-09-19 10:27:01 -0500

    class work

---
+++ events.js
@@ -0,0 +1,35 @@
+function showVal()
+{
+    console.log(document.getElementById('name').value);
+    alert("hello");
+}
+//first parameter is what command for event listener, then the function which holds the event
+document.getElementById('theButton').addEventListener("click", showVal);
+var textBox = document.getElementById('name');
+textBox.addEventListener("focus", newFunction);
+
+function newFunction()
+{
+    textBox.style.backgroundColor = "green";
+
+}
+
+var secondButton = document.getElementById('button2');
+secondButton.addEventListener("click", function() {
+   var divText = document.getElementById('theDiv').innerHTML;
+   divText += "new stuff";
+   document.getElementById('theDiv').innerHTML = divText;
+   alert(divText);
+});
+
+document.getElementById('lists').addEventListener("click", function() {
+var listItems = document.getElementsByTagName('li');
+var addedItem = document.getElementById('addAnItem').value;
+listItems += addedItem;
+for(let i = 0; i < listItems.length; i++)
+{
+    console.log(listItems[i].innerHTML);
+}
+document.getElementById('theList').innerHTML += "<li>" + addedItem + "</li>";
+
+});
